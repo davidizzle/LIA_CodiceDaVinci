17bea6bb3bc5e1d355b65feb82c2b287f3199ee1
davidizzle <davide.ferretti.j@gmail.com>
2025-04-19 15:54:11 +0200

    Model upgrade and GPU Support

--- app.py
+++ app.py
@@ -3,17 +3,24 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 
 # deepseek-ai/DeepSeek-Coder-V2-Lite-Instruct
-model_id = "deepseek-ai/deepseek-coder-1.3b-instruct"
+# model_id = "deepseek-ai/deepseek-coder-1.3b-instruct"
+model_id = "deepseek-ai/deepseek-coder-6.7b-instruct"
 tokenizer = AutoTokenizer.from_pretrained(model_id)  # Or your own!
-model = AutoModelForCausalLM.from_pretrained(model_id, device_map=None, torch_dtype=torch.float32, trust_remote_code=True)
-model.to("cpu")
+model = AutoModelForCausalLM.from_pretrained(model_id, 
+                                            #  device_map=None, 
+                                            #  torch_dtype=torch.float32, 
+                                             device_map="auto", 
+                                             torch_dtype=torch.float16, 
+                                             trust_remote_code=True)
+# model.to("cpu")
 
 def generate_code(prompt, style="Clean & Pythonic"):
     if style == "Verbose like a 15th-century manuscript":
         prompt = "In a manner most detailed, write code that... " + prompt
     inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
     outputs = model.generate(**inputs, 
-                             max_new_tokens=100,
+                            #  max_new_tokens=100,
+                             max_new_tokens=300,
                             do_sample=True,
                             temperature=1.0,
                             top_p=0.95,
